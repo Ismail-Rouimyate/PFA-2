4ff08889339535c7650363d3c048613a6df19cd4
Ismail-Rouimyate <Ismail.95r@gmail.com>
2019-06-19 23:43:42 +0100

    Add files via upload

--- datasetConverter.py
+++ datasetConverter.py
@@ -1,22 +1,32 @@
 import os
 import numpy as np
 import cv2
+import pyarabic
+from openpyxl import load_workbook
 
 class DataProvider():
     "this class creates machine-written text for a word list. TODO: change getNext() to return your samples."
+    
+            
 
-    def __init__(self, wordList):
+
+
+    
+
+    def __init__(self, wordList, imageList):
+        
         self.wordList = wordList
         self.idx = 0
+        self.imageList = imageList
 
     def hasNext(self):
         return self.idx < len(self.wordList)
 
     def getNext(self):
-        img = np.ones((32, 128), np.uint8)*255    
+        
+        img = cv2.imread(self.imageList[self.idx],0)    
         word = self.wordList[self.idx]
         self.idx += 1
-        cv2.putText(img,word,(2,20), cv2.FONT_HERSHEY_SIMPLEX, 0.4, (0), 1, cv2.CV_AA)
         return (word, img)
 
 
@@ -36,7 +46,7 @@ def createIAMCompatibleDataset(dataProvider):
         sample = dataProvider.getNext()
 
         # write img
-        cv2.imwrite('sub/sub-sub/sub-sub-%d.png'%ctr, sample[1])
+        cv2.imwrite('sub/sub-sub-%d.png'%ctr, sample[1])
 
         # write filename, dummy-values and text
         line = 'sub-sub-%d'%ctr + ' X X X X X X X ' + sample[0] + '\n'
@@ -46,6 +56,19 @@ def createIAMCompatibleDataset(dataProvider):
 
 
 if __name__ == '__main__':
-    words = ['some', 'words', 'for', 'which', 'we', 'create', 'text-images']
-    dataProvider = DataProvider(words)
+    words = []
+    filename = "separated-test.txt"
+    file = open(filename,"r")
+    content = file.readlines()
+    for i in content :
+        words.append(i[24:])
+    file.close()
+    images = []
+    filename = "separated-test.txt"
+    file = open(filename,"r")
+    content = file.readlines()
+    for i in content :
+        images.append(i[0:22])
+    file.close()
+    dataProvider = DataProvider(words,images)
     createIAMCompatibleDataset(dataProvider)
